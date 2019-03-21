99af74c3089427246ca528f5092f8ad9e97911a6
阿部寛子 <hihoyeahi@gmail.com>
2019-03-21 10:22:51 +0900

    wp環境構築の為index.php作りました

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>北谷メディアセンター</title>
+
+  <!-- Favicons ============================================= -->
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+  <!-- Stylesheets ============================================= -->
+
+  <!-- 阿部さんcss -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style_sample.css">
+
+  <!-- Default stylesheets-->
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link href="assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Template specific stylesheets-->
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+  <link href="assets/lib/animate.css/animate.css" rel="stylesheet">
+  <link href="assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
+  <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
+  <link href="assets/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+  <link href="assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+  <link href="assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+  <!-- Main stylesheet and color file-->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
+  <main>
+
+    <!-- レンタル会議室 -->
+    <h1>レンタル会議室</h1>
+    <p>1時間あたり1,200円からご案内しています。</p>
+    <div class="img">
+      <img src="assets/images/meeting-room/IT.JPG" alt="IT-room">
+      <img src="assets/images/meeting-room/meeting-roomA.JPG" alt="meeting-roomA">
+      <img src="assets/images/meeting-room/meeting-roomB.JPG" alt="meeting-roomB">
+    </div>
+
+
+    <!-- レンタルオフィス -->
+    <h1>レンタルオフィス</h1>
+    <p>レンタルオフィスは北谷町役場商工観光課より公募という形で募集をおこないます</p>
+    <div class="img">
+      <img src="assets/images/meeting-room/office-roomA.JPG" alt="office-roomA">
+      <img src="assets/images/meeting-room/office-roomB.JPG" alt="office-roomB">
+      <img src="assets/images/meeting-room/office-roomC.JPG" alt="office-roomC">
+    </div>
+
+
+    <!-- レンタル器材 -->
+    <section class="module" id="rental">
+      <h2 class="subtitle">レンタル機材</h2>
+      <h3>ご予約は本日より７営業日先から受付をおこない、向こう3ヶ月までご予約できます。</h3>
+      <ul class="works-grid works-grid-gut works-grid-3 works-hover-w" id="works-grid work-item">
+        <li class="work-item"><a href="portfolio-single-1.html">
+          <div class="work-image"><img src="https://placehold.jp/311.33px383.png" alt="Portfolio Item"/></div>
+          <div class="work-caption font-alt">
+            <h3 class="work-title">1日料金：2,600円</h3>
+            <h4 class="work-descr">詳細はこちらから</h4>
+          </div></a>
+        <span>ビンデン三脚</span>
+        </li>
+        <li class="work-item"><a href="portfolio-single-1.html">
+          <div class="work-image"><img src="https://placehold.jp/311.33px383.png" alt="Portfolio Item"/></div>
+          <div class="work-caption font-alt">
+            <h3 class="work-title">1日料金：500円</h3>
+            <h4 class="work-descr">詳細はこちらから</h4>
+          </div></a>
+          <span>マンフロット三脚</span>
+        </li>
+        <li class="work-item"><a href="portfolio-single-1.html">
+          <div class="work-image"><img src="https://placehold.jp/311.33px383.png" alt="Portfolio Item"/></div>
+          <div class="work-caption font-alt">
+            <h3 class="work-title">1日料金：500円</h3>
+            <h4 class="work-descr">詳細はこちらから</h4>
+          </div></a>
+        <span>ドリー</span>
+        </li>
+        <li class="work-item"><a href="portfolio-single-1.html">
+          <div class="work-image"><img src="https://placehold.jp/311.33px383.png" alt="Portfolio Item"/></div>
+          <div class="work-caption font-alt">
+            <h3 class="work-title">1日料金：2,200円</h3>
+            <h4 class="work-descr">詳細はこちらから</h4>
+          </div></a>
+          <span>ライティングキット</span>
+        </li>
+      </ul>
+    </section>  
+
+
+    <!-- レンタルスタジオ -->
+    <section class="module" id="studio">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <img src="assets/images/studio_slide/studio-1.jpg" alt="">
+          </div>
+          <div class="col-sm-6">
+            <div class="table-container">
+              <h2>レンタルスタジオ</h2>
+              <p>広さ：６０坪</p>
+              <table>
+                <tr>
+                  <td></td>
+                  <td>9-19時/時間</td>
+                  <td>19-9時/時間</td>
+                  <td>1日/24時間</td>
+                </tr>
+                <tr>
+                  <td>スタジオ</td>
+                  <td>¥4,400</td>
+                  <td>¥5,700</td>
+                  <td>¥104,500</td>
+                </tr>
+                <tr>
+                  <td>照明設備</td>
+                  <td>¥5,400</td>
+                  <td>¥7,000</td>
+                  <td>¥129,400</td>
+                </tr>
+                <tr>
+                  <td>音響設備</td>
+                  <td></td>
+                  <td></td>
+                  <td>¥6,300</td>
+                </tr>
+              </table>
+              <p class="link"><a href="">スタジオ</a>照明設備/図面<a href="">PDFダウンロード</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- メディアステーションのブログ -->
+    <section class="module" id="news">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-sm-offset-3">
+            <h2 class="module-title font-alt">メディアステーションのブログ</h2>
+            <div class="module-subtitle font-serif">当ステーションの新鮮な情報を掲載していきます。<br>パソコン講座の案内、撮影機材に関すること、その他 日常の出来事など。お楽しみに！</div>
+          </div>
+        </div>
+        <!-- イメージ画像 -->
+        <div class="row multi-columns-row post-columns">
+          <!-- １記事目 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="post mb-20">
+              <div class="post-thumbnail"><a href="#"><img src="assets/images/post-1.jpg" alt="Blog-post Thumbnail"/></a></div>
+              <div class="post-header font-alt">
+                <h2 class="post-title"><a href="#">タイトル</a></h2>
+                <div class="post-meta"><p>2019/03/17</p>
+                </div>
+              </div>
+              <div class="post-entry">
+                <p>記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。</p>
+              </div>
+              <div class="post-more"><a class="more-link" href="#">続きを読む</a></div>
+            </div>
+          </div>
+          <!-- ２記事目 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="post mb-20">
+              <div class="post-thumbnail"><a href="#"><img src="assets/images/post-2.jpg" alt="Blog-post Thumbnail"/></a></div>
+              <div class="post-header font-alt">
+                <h2 class="post-title"><a href="#">タイトル</a></h2>
+                <div class="post-meta"><p>2019/03/17</p>
+                </div>
+              </div>
+              <div class="post-entry">
+                <p>記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。</p>
+              </div>
+              <div class="post-more"><a class="more-link" href="#">続きを読む</a></div>
+            </div>
+          </div>
+          <!-- ３記事目 -->
+          <div class="col-sm-6 col-md-4 col-lg-4">
+            <div class="post mb-20">
+              <div class="post-thumbnail"><a href="#"><img src="assets/images/post-3.jpg" alt="Blog-post Thumbnail"/></a></div>
+              <div class="post-header font-alt">
+                <h2 class="post-title"><a href="#">タイトル</a></h2>
+                <div class="post-meta"><p>2019/03/17</p>
+                </div>
+              </div>
+              <div class="post-entry">
+                <p>記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。記事の抜粋がここに表示されます。</p>
+              </div>
+              <div class="post-more"><a class="more-link" href="#">続きを読む</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="btn">
+          <button>記事一覧</button>
+        </div>
+      </div>
+    </section>
+
+    <!-- googleマップ -->
+    <section class="module" id="map">
+      <div class="container">
+        <div class="row">
+          <h2 class="module-title font-alt">アクセス</h2>
+          <div class="google-map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.3207843680784!2d127.75571131557224!3d26.31611028338815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34e513051b6aba63%3A0x758d0e90af22e324!2z5YyX6LC355S6576O5rWc44Oh44OH44Kj44Ki44K544OG44O844K344On44Oz!5e0!3m2!1sja!2sjp!4v1553078249420" width="768" height="576" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- コンタクトフォーム -->
+    <section class="module" id="contact">
+      <div class="container">
+        <!-- タイトルテキスト部分 -->
+        <div class="row">
+          <div class="col-sm-6 col-sm-offset-3">
+            <h2 class="module-title font-alt">お問い合わせ</h2>
+            <p>北谷町美浜メディアステーション管理事務所</p>
+            <p>電話：098-926-5140（平日9時～17時）</p>
+            <p>FAX：098-926-5140</p>
+            <div class="module-subtitle font-serif"></div>
+          </div>
+        </div>
+        <!-- 入力フォーム部分 -->
+        <div class="row">
+          <div class="col-sm-6 col-sm-offset-3">
+            <form id="contactForm" role="form" method="post" action="php/contact.php">
+              <!-- 名前入力フォーム -->
+              <div class="form-group">
+                <label class="sr-only" for="name">お名前</label>
+                <input class="form-control" type="text" id="name" name="name" placeholder="お名前" required="required" data-validation-required-message="Please enter your name."/>
+                <p class="help-block text-danger"></p>
+              </div>
+              <!-- E-mail 入力フォーム -->
+              <div class="form-group">
+                <label class="sr-only" for="email">メールアドレス</label>
+                <input class="form-control" type="email" id="email" name="email" placeholder="メールアドレス" required="required" data-validation-required-message="Please enter your email address."/>
+                <p class="help-block text-danger"></p>
+              </div>
+              <!-- 内容入力フォーム -->
+              <div class="form-group">
+                <textarea class="form-control" rows="7" id="message" name="message" placeholder="お問い合わせ内容" required="required" data-validation-required-message="Please enter your message."></textarea>
+                <p class="help-block text-danger"></p>
+              </div>
+              <div class="text-center">
+                <button class="btn btn-block btn-round btn-d" id="cfsubmit" type="submit">送信</button>
+              </div>
+            </form>
+            <div class="ajax-response font-alt" id="contactFormResponse"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr class="divider-d">
+
+    <!-- フッター -->
+    <footer class="footer bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <p class="copyright font-alt"><a href="">北谷町美浜メディアステーション管理事務所 ALL RIGHT RESERVED</p></a>
+          </div>
+          <div class="col-sm-6">
+            <a href="">北谷町役場HP</a>
+            <a href="">北谷町観光協会HP</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </main>
+
+  <!-- JavaScripts ============================================= -->
+  <script src="assets/lib/jquery/dist/jquery.js"></script>
+  <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="assets/lib/wow/dist/wow.js"></script>
+  <script src="assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
+  <script src="assets/lib/isotope/dist/isotope.pkgd.js"></script>
+  <script src="assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
+  <script src="assets/lib/flexslider/jquery.flexslider.js"></script>
+  <script src="assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
+  <script src="assets/lib/smoothscroll.js"></script>
+  <script src="assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
+  <script src="assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
