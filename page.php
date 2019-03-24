9670464963cf4c7fab09933f2afb820824679e3f
阿部寛子 <hihoyeahi@gmail.com>
2019-03-24 16:23:30 +0900

    header 付けました

--- page.php
+++ page.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
+<?php get_header(); ?>
+
+
+<!-- <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--  
+ -->    <!--  
     Document Title
     =============================================
     -->
@@ -89,4 +92,5 @@
     </div>
 
 </body>
-</html>
+
+<?php get_footer(); ?>
