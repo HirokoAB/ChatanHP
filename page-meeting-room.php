4e38eb628a193113f703b491294824f0e0f99758
Atsushi <atsushi88885@gmail.com>
2019-03-31 16:42:04 +0900

    固定ページ作成

--- page-meeting-room.php
+++ page-meeting-room.php
@@ -1,13 +1,10 @@
 <?php get_header(); ?>
 
-<body>
-<!-- レンタル会議室 -->
+<section class="meeting-room">
   <h1>レンタル会議室</h1>
   <h2>1時間あたり1,200円からご案内しています。</h2>
-<!-- 会議室１つ目ここから -->
   <div class="it-room">
-    <img src="img/IT.JPG" alt="IT-room">
-
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/IT.JPG" alt="IT-room">
     <div class="text">
       <p class="it">IT研修室</p>
       <p>広さ：55㎡</p>
@@ -16,9 +13,6 @@
       <p>町内料金/時間：¥1,800</p>
     </div>
   </div>
-
-<!-- 会議室１つ目ここまで -->
-<!-- 会議室２つ目ここから -->
   <div class="it-room">
     <div class="text">
       <p class="it">IT研修室</p>
@@ -27,12 +21,10 @@
       <P>町内料金/時間：¥1,200</P>
       <p>町内料金/時間：¥1,800</p>
     </div>
-    <img src="img/meeting-roomA.JPG" alt="meeting-roomA">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting-roomA.JPG" alt="meeting-roomA">
   </div>
-<!-- 会議室２つ目ここまで -->
-<!-- 会議室3つ目ここから -->
   <div class="it-room">
-    <img src="img/meeting-roomB.JPG" alt="meeting-roomB">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting-roomB.JPG" alt="meeting-roomB">
     <div class="text">
       <p class="it">IT研修室</p>
       <p>広さ：55㎡</p>
@@ -41,6 +33,6 @@
       <p>町内料金/時間：¥1,800</p>
     </div>
   </div>
-<!-- 会議室3つ目ここまで -->
+</section>
 
 <?php get_footer(); ?>
--- page-office.php
+++ page-office.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section class="rental">
+<section class="office">
    <!-- レンタルオフィス -->
 
   <h1>レンタルオフィス</h1>
