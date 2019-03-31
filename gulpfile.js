aa1e3e9399397d95cd36dee3b10828e75eb04d1c
Atsushi <atsushi88885@gmail.com>
2019-03-31 11:52:48 +0900

    sassをコンパイルできるよう修正

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
