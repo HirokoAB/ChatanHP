cb0c122acd2c8d05bad34e1a0581fc5311e7f4dd
Atsushi <atsushi88885@gmail.com>
2019-04-15 21:55:11 +0900

    新しいパッケージ導入

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sassGlob = require('gulp-sass-glob');
+var sourcemaps =require('gulp-sourcemaps'); 
+var autoprefixer =require('gulp-autoprefixer'); 
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init()) 
     .pipe(sassGlob())
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"],
+      cascade: false
+    }))   
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./assets/css'));
 });
 
