80444fa2b9fe67927fe70eb5367d23792a9f53c5
阿部寛子 <hihoyeahi@gmail.com>
2019-04-30 12:06:48 +0900

    ブログ詳細ページ作成 #50 ・fontawsome 使うためにheader 触りました ・レイアウトはXDを参照しました

--- single.php
+++ single.php
@@ -1,41 +1,75 @@
 <?php get_header(); ?>
 
+
 <?php if(have_posts()): the_post(); ?>
-<article <?php post_class(); ?>>
-  <!--投稿日・著者を表示-->
-  <div class="kiji-info">
-    <!--投稿日を取得-->
-    <span class="kiji-date">
-      <i class="fas fa-pencil-alt"></i>
-      <time
-      datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
-      <?php echo get_the_date(); ?>
-      </time>
-    </span>
-    <!--カテゴリ取得-->
-    <?php if(has_category() ): ?>
-    <span class="cat-data">
-      <?php echo get_the_category_list( ' ' ); ?>
-    </span>
-    <?php endif; ?>
-  </div>
-  <!--タイトル-->
-  <h1><?php the_title(); ?></h1>
-  <!--アイキャッチ取得-->
-  <?php if( has_post_thumbnail() ): ?>
-  <div class="kiji-img">
-    <?php the_post_thumbnail( 'large' ); ?>
-  </div>
-  <?php endif; ?>
-  <!--本文取得-->
-  <?php the_content(); ?>
-  <!--タグ-->
-  <div class="">
-    <?php the_tags('<ul><li>タグ： </li><li>','</li><li>','</li></ul>'
-  ); ?>
-  </div>
-</article>
+
+  <article> <!-- <?php post_class( ); ?> -->
+
+      <div class ="article_container"> 
+        <!--投稿日・著者を表示-->
+        <div>
+            <!--カテゴリ取得-->
+            <?php if(has_category() ): ?>
+              <span class="cat-data">
+                <?php echo the_category(''); ?>
+              </span>
+            <?php endif; ?>
+        </div>
+        <!--アイキャッチ取得-->
+        <?php if( has_post_thumbnail() ): ?>
+          <div class="article_img">
+            <?php the_post_thumbnail('large'); ?>
+          </div>
+        <?php endif; ?> 
+        <div class= "article_title">       
+          <!--タイトルを取得-->
+          <h1><?php the_title(); ?></h1>
+          <!--投稿日を取得-->
+          <span class="article_date">
+            <i class="fas fa-pencil-alt"></i>
+              <time
+            datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
+              <?php echo get_the_date(); ?>
+            </time>
+          </span>
+        </div>
+        <!--本文取得-->
+        <div class="article_content">
+          <?php the_content(); ?>
+        </div>
+
+  <?php else: ?>
+
+    <p>投稿はありません</p>
+
 <?php endif; ?>
+
+
+<div class="nav-below">
+  <span class="pre">
+    <?php previous_post_link('%link','< 前へ'); ?>
+  </span>
+  <span class="next">  
+    <?php next_post_link('%link','次へ >'); ?>
+  </span>
+</div>
+</div>
+ 
+<div class="article_archive">
+  <p class="latest_title">最新の投稿</p>
+  <div class="article_loop">
+    <?php $paged = get_query_var('paged'); ?>
+    <?php query_posts("posts_per_page=3&paged+$paged"); ?>
+    <?php get_template_part('loop', 'main'); ?>
+  </div> 
+
+  <div class="list-btn">
+    <a href="<?php echo get_permalink( BLOG ); ?>">記事一覧</a>
+  </div>
+
+</div>
+
+</article>  
 
     
 <?php get_footer(); ?>
